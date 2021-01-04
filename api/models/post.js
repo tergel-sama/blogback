977eab69fd8a9b848e0a62cb86fe014dd1884738
tergel-sama <58690404+tergel-sama@users.noreply.github.com>
2021-01-05 00:04:47 +0800

    Update post.js

--- api/models/post.js
+++ api/models/post.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 const postSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: String,
-  img:String,
+  img: String,
   content: Object,
   author: String,
-  tags:Array
+  tags: Array,
+  approve: { type: Number, default: 0 },
+  views: { type: Number, default: 0 },
+  likes:{ type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("post", postSchema);
